aa2a0f2658fe7dd95d5739309bc90a408fbf33c0
Yu Hoshino <yhoshino11@gmail.com>
2015-05-14 00:06:31 +0900

    Started Re-learning Land of Lisp.

---
+++ ch02/guess-my-number.lisp
@@ -0,0 +1,5 @@
+; To Load This File.
+; (load 'guess-my-number.lisp)
+
+(defparameter *small* 1)
+(defparameter *big* 100)
---
+++ ch02/practice.lisp
@@ -0,0 +1,9 @@
+; defparameter allows override
+; (defparameter *foo* 5)
+; (defparameter *foo* 6)
+; *foo*
+
+; defvar doesn't allow override
+; (defvar *foo* 5)
+; (defvar *foo* 6)
+; *foo*

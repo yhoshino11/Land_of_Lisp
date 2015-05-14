f7266a0443d611f1a36f8e17bfedf071140ed38a
Yu Hoshino <yhoshino11@gmail.com>
2015-05-14 16:29:42 +0900

    Chapter 6 done. (game-repl) is REPL version of ch05's adventure_game.lisp

--- ch06/practice.lisp
+++ ch06/practice.lisp
@@ -92,3 +92,72 @@
 ; (game-repl) # Ctrl-C to exit
 ; (+ 1 1)
 ; 1
+
+
+; # Before use geme-x functions, load adventure_game.lisp first.
+(load '../ch05/adventure_game.lisp)
+
+(defun game-read ()
+  (let ((cmd (read-from-string
+               (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+                     (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+; (game-read)
+; walk east
+; (WALK 'EAST)
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+    (eval sexp)
+    '(i do not know that command.)))
+; (game-eval '(pickup 'whiskey))
+; returns (YOU ARE NOW CARRYING THE WHISKEY)
+
+; (game-eval '(list))
+; returns (I DO NOT KNOW THAT COMMAND.)
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eq item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                 nil)
+         'string))
+  (fresh-line))
+; (game-print '(not only "comma," it also mentions the "iPad."))
+; Not only comma, it also mentions the iPad.
+
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
+
+; To Play Game with REPL
+; (game-repl) # type below commands interactively.
+; look
+; walk west
+; pickup chain
+; scratch head
+; pickup chicken
+; walk east
+; walk upstairs
+; inventory
+; walk china
+; walk downstairs
+; pickup bucket
+; look
+; quit

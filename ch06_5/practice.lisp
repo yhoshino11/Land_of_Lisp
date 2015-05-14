43907efd682873eab4fe6ed204bdbc0654ac2f88
Yu Hoshino <yhoshino11@gmail.com>
2015-05-14 17:16:51 +0900

    Add Lambda code.

---
+++ ch06_5/practice.lisp
@@ -0,0 +1,14 @@
+; (defun half (n)
+  ; (/ n 2))
+; (half 2)
+; returns 1
+
+; #'half
+; returns #<FUNCTION HALF (N)...>
+
+; (lambda (n) (/ n 2))
+; returns #<FUNCTION :LAMBDA (N) (/ N 2)>
+
+; (mapcar (lambda (n) (/ n 2))
+        ; '(2 4 6))
+; returns (1 2 3)

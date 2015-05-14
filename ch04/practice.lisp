4ab8930e6cb4eaeccefa0cfa29c189870dcc6bb9
Yu Hoshino <yhoshino11@gmail.com>
2015-05-14 12:16:56 +0900

    Chapter 4 done.

--- ch04/practice.lisp
+++ ch04/practice.lisp
@@ -91,3 +91,60 @@
     ; ((johnny) (setf *arch-enemy* 'useless-old-johnny)
               ; '(i hope you choked on my pudding johnny))
     ; (otherwise '(why you eat my pudding stranger ?))))
+
+; returns T
+; (and (oddp 5) (oddp 7) (oddp 9))
+
+; returns T
+; (or (oddp 4) (oddp 7) (oddp 8))
+
+; returns T & changes *is-it-even* to be T
+; (defparameter *is-it-even* nil)
+; (or (oddp 4) (setf *is-it-even* t))
+
+; returns T, but *is-it-even* stays nil. (setf *is-it-even* t) won't be executed.
+; (defparameter *is-it-even* nil)
+; (or (oddp 5) (setf *is-it-even* t))
+
+; returns ONE-IS-IN-THE-LIST
+; (if (member 1 '(3 4 1 5))
+  ; 'one-is-in-the-list
+  ; 'one-is-not-in-the-list)
+
+; returns (1 5)
+; (member 1 '(3 4 1 5))
+
+; returns NIL-IS-IN-THE-LIST
+; (if (member nil '(3 4 nil 5))
+  ; 'nil-is-in-the-list
+  ; 'nil-is-not-in-the-list)
+
+; returns 5
+; (find-if #'oddp '(2 4 5 6))
+
+; returns THERE-IS-AN-ODD-NUMBER
+; (if (find-if #'oddp '(2 4 5 6))
+  ; 'there-is-an-odd-number
+  ; 'there-is-no-odd-number)
+
+; returns nil
+; (find-if #'null '(2 4 nil 6))
+
+; returns ITS-AN-APPLE
+; (defparameter *fruit* 'apple)
+; (cond ((eq *fruit* 'apple) 'its-an-apple)
+      ; ((eq *fruit* 'orange) 'its-an-orange))
+
+; returns T
+; (equal 'apple 'apple)
+; (equal (list 1 2 3) (list 1 2 3))
+; (equal '(1 2 3) (cons 1 (cons 2 (cons 3 ()))))
+; (equal 5 5)
+; (equal 2.5 2.5)
+; (equal "foo" "foo")
+; (equal #\a #\a)
+; (eql 'foo 'foo)
+; (eql 3.4 3.4)
+; (eql #\a #\a)
+; (equalp "Bob Smith" "bob smith")
+; (equalp 0 0.0)

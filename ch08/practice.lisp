485745bd451da07aeda7ee355362617d11909568
Yu Hoshino <yhoshino11@gmail.com>
2015-05-16 11:47:06 +0900

    Practiced difference between let & let*

--- ch08/practice.lisp
+++ ch08/practice.lisp
@@ -14,3 +14,17 @@
       collect (+ 100 n))
 ; returns
 ; (101 102 103 104 105 106 107 108 109 110)
+
+
+; (let ((a 5)
+      ; (b (+ a 2)))
+  ; b)
+; returns ERROR
+; *** = LET: variable A has no value
+
+
+(let* ((a 5)
+       (b (+ a 2)))
+  b)
+; returns
+; 7
